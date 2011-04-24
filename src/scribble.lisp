2140006805477ca8f0f3e202a7f68b711da70287
Valeriy Fedotov <valeriy.fedotov@gmail.com>
2011-04-24 21:21:12 +0400

    Added canvas dragging, preparations for scaling.

--- src/scribble.lisp
+++ src/scribble.lisp
@@ -9,27 +9,54 @@
   (cffi:with-foreign-string (foreign-name name)
     (gtk_rc_parse foreign-name)))
 
+(defvar *zoom-sequence* '(1/4 1/2 2/3 1 4/3 3/2 2 4 8 16))
+
+(defclass zoomer ()
+  ((value :initform 1 :accessor value)
+   (zoom-index :initform (position 1 *zoom-sequence* :test #'=) :accessor zoom-index)))
+
+(defun zoom-in (zoomer)
+  (let ((next-index (1+ (zoom-index zoomer))))
+    (when (< next-index (length *zoom-sequence*))
+      (incf (zoom-index zoomer))
+      (setf (value zoomer) (elt *zoom-sequence* (zoom-index zoomer)))))
+  (value zoomer))
+
+(defun zoom-out (zoomer)
+  (let ((next-index (1- (zoom-index zoomer))))
+    (when (>= next-index 0)
+      (decf (zoom-index zoomer))
+      (setf (value zoomer) (elt *zoom-sequence* (zoom-index zoomer)))))
+  (value zoomer))
+
 ;; FIXME: cleanup
 
 (defparameter *canvas-margin* 400
   "Margin around painting in GtkFixed.")
 
 (defun scribble-xinput ()
-  (let (pixmap
+  (let (backing-pixmap
+	canvas-pixbuf
 	input-d
 	brush-stroke
 	hsv
 	viewport
 	spacing-adjustment
 	alpha-adjustment
+	vadjustment
+	hadjustment
 	fullscreen-p
 	brush
 	da0
 	fixed
-	(renew-pixmap-p t)
-	(builder (let ((builder (make-instance 'builder)))
-		   (builder-add-from-file builder (namestring (merge-pathnames "ui/mainwin.glade" *src-location*)))
-		   builder)))
+	drag-mode-p
+	drag-p
+	drag-x0
+	drag-y0
+	(zoomer (make-instance 'zoomer))
+	(renew-backing-pixmap-p t)
+	(builder (make-instance 'builder :from-file
+				(namestring (merge-pathnames "ui/mainwin.glade" *src-location*)))))
     (labels ((bgo (object-name) (builder-get-object builder object-name))
 	     (realize (widget)
 	       (iter (for i in '(:pointer-motion-mask :pointer-motion-hint-mask
@@ -37,85 +64,82 @@
 		     (pushnew i (gdk-window-events (widget-window widget)))))
 	     (configure-event (widget event)
 	       (declare (ignore event))
-	       (when renew-pixmap-p
-		 (setf renew-pixmap-p nil)
+	       (when renew-backing-pixmap-p
+		 (setf renew-backing-pixmap-p nil)
 		 (let* ((window (widget-window widget))
 			(gc (graphics-context-new window)))
 		   (multiple-value-bind (w h) (gdk:drawable-get-size (widget-window widget))
 		     (print (list 'conf w h) *debug*)
-		     (setf pixmap (pixmap-new window w h -1))
+		     (setf backing-pixmap (pixmap-new window w h -1))
 		     (setf (graphics-context-rgb-fg-color gc)
 			   (make-color :red 65535 :green 65535 :blue 65535))
-		     (gdk::draw-rectangle pixmap gc t 0 0 w h)))
+		     (gdk::draw-rectangle backing-pixmap gc t 0 0 w h)))
 		 t))
 	     (expose-event (widget event)
-	       (let* ((window (widget-window widget))
-		      (gc (graphics-context-new window))
-		      (rect (event-expose-area event)))
-		 (setf (graphics-context-rgb-fg-color gc)
-		       (make-color :red 0000 :green 0000 :blue 0000))
-		 (draw-rectangle window gc t (rectangle-x rect) (rectangle-y rect)
-				 (rectangle-width rect) (rectangle-height rect)))
 	       (let* ((rect (event-expose-area event))
 		      (window (widget-window widget))
 		      (gc (graphics-context-new window)))
-		 (draw-drawable window gc pixmap
+		 (draw-drawable window gc backing-pixmap
 				(rectangle-x rect) (rectangle-y rect)
 				(rectangle-x rect) (rectangle-y rect)
 				(rectangle-width rect) (rectangle-height rect)))
 	       nil)
 	     (button-press-event (widget event)
-	       (when (and pixmap
-			  (= (event-button-button event) 1))
-		 (let ((pressure (event-get-axis event :pressure))
-		       (source (gdk-device-source (event-button-device event))))
-		   (setf brush-stroke (make-instance 'brush-stroke :pressure (if (eq :mouse source)
-										 (clamp (or pressure 1) 0 1)
-										 (clamp (or pressure 0) 0 1))
-						     :x (event-button-x event) :y (event-button-y event)
-						     :brush brush
-						     :color (bind:bind (((:values h s v) (h-s-v-get-color hsv))
-									((:values r g b) (h-s-v-to-r-g-b h s v))
-									(r1 (* 65535 r))
-									(g1 (* 65535 g))
-									(b1 (* 65535 b)))
-							      (print (list r g b) *debug*)
-							      (make-color :red r1 :green g1 :blue b1))
-						     #+ nil (flet ((greyscale-color (x)
-								     (make-color :red x :green x :blue x)))
-							      (greyscale-color
-							       (ecase source
-								 (:mouse 15000)
-								 (:pen 0)
-								 (:eraser 65535)
-								 (:cursor 45000))))
-						     :spacing (adjustment-value spacing-adjustment)
-						     :alpha (print (adjustment-value alpha-adjustment) *debug*)
-						     :pixmap pixmap :widget widget)))))
+	       (print (list 'abc (event-button-button event)) *debug*)
+	       (if (or drag-p (= (event-button-button event) 2))
+		   (setf drag-p t
+			 drag-x0 (event-button-x event)
+			 drag-y0 (event-button-y event))
+		   (when (and backing-pixmap
+			      (= (event-button-button event) 1))
+		     (let ((pressure (event-get-axis event :pressure))
+			   (source (gdk-device-source (event-button-device event))))
+		       (setf brush-stroke (make-instance 'brush-stroke :pressure (if (eq :mouse source)
+										     (clamp (or pressure 1) 0 1)
+										     (clamp (or pressure 0) 0 1))
+							 :x (event-button-x event) :y (event-button-y event)
+							 :brush brush
+							 :color (bind:bind (((:values h s v) (h-s-v-get-color hsv))
+									    ((:values r g b) (h-s-v-to-r-g-b h s v))
+									    (r1 (* 65535 r))
+									    (g1 (* 65535 g))
+									    (b1 (* 65535 b)))
+								  (print (list r g b) *debug*)
+								  (make-color :red r1 :green g1 :blue b1))
+							 :spacing (adjustment-value spacing-adjustment)
+							 :alpha (print (adjustment-value alpha-adjustment) *debug*)
+							 :pixmap backing-pixmap :widget widget))))))
 	     (button-release-event (widget event)
 	       (declare (ignore widget event))
-	       (setf brush-stroke nil))
+	       (setf brush-stroke nil
+		     drag-p nil))
 	     (motion-notify-event (widget event)
-	       (let ((device (event-motion-device event))
-		     x y pressure state)
-		 (if (= (event-motion-is-hint event) 1)
-		     (setf x (event-get-axis event :x)
-			   y (event-get-axis event :y)
-			   pressure (event-get-axis event :pressure)
-			   state (event-motion-state event))
-		     (multiple-value-bind (retval x1 y1 state1)
-			 (gdk-window-get-pointer (widget-window widget))
-		       (declare (ignore retval))
-		       (setf x x1 y y1 
-			     pressure (event-get-axis event :pressure)
-			     state state1)))
-		 (if (and pixmap
-			  (member :button1-mask state))
-		     (when brush-stroke
-		       (add-point-to-stroke brush-stroke x y (if (eq :mouse (gdk-device-source device))
-								 (clamp (or pressure 1) 0 1)
-								 (clamp (or pressure 0) 0 1)))
-		       (draw-stroke brush-stroke)))))
+	       (print 'move *debug*)
+	       (if drag-p
+		   (progn
+		     (print (- (event-motion-x event) drag-x0) *debug*)
+		     (setf (adjustment-value hadjustment) (+ (adjustment-value hadjustment) (- drag-x0 (event-motion-x event)))
+	 		   (adjustment-value vadjustment) (+ (adjustment-value vadjustment) (- drag-y0 (event-motion-y event)))))
+		   (let ((device (event-motion-device event))
+			 x y pressure state)
+		     (if (= (event-motion-is-hint event) 1)
+			 (setf x (event-get-axis event :x)
+			       y (event-get-axis event :y)
+			       pressure (event-get-axis event :pressure)
+			       state (event-motion-state event))
+			 (multiple-value-bind (retval x1 y1 state1)
+			     (gdk-window-get-pointer (widget-window widget))
+			   (declare (ignore retval))
+			   (setf x x1 y y1 
+				 pressure (event-get-axis event :pressure)
+				 state state1)))
+		     (if (and backing-pixmap
+			      (member :button1-mask state))
+			 (when brush-stroke
+			   (add-point-to-stroke brush-stroke x y (if (eq :mouse (gdk-device-source device))
+								     (clamp (or pressure 1) 0 1)
+								     (clamp (or pressure 0) 0 1)))
+			   (draw-stroke brush-stroke))))))
 	     (key-press-event (window event)
 	       (if (eql (event-key-keyval event) 65480)
 		   (if fullscreen-p
@@ -138,33 +162,35 @@
 		     (combo-box-active c) 0))
 	     (file-save (a)
 	       (declare (ignore a))
-	       (let ((d (make-instance 'file-chooser-dialog :action :save :title "Choose file to save")))
+	       (let ((d (make-instance 'file-chooser-dialog :action :save :title "Choose file name for saving")))
 		 (dialog-add-button d "gtk-save" :accept)
 		 (dialog-add-button d "gtk-cancel" :cancel)
 		 (when (eq (dialog-run d) :accept)
-		   (pixbuf-save (pixbuf-get-from-drawable nil pixmap) (file-chooser-filename d) "png")
+		   (pixbuf-save (pixbuf-get-from-drawable nil backing-pixmap) (file-chooser-filename d) "png")
 		   (format *debug* "saved to file ~A~%" (file-chooser-filename d)))
 		 (object-destroy d)))
+	     (change-canvas-size (w h gc &optional pixbuf)
+	       (setf (widget-width-request da0) w
+		     (widget-height-request da0) h)
+	       (setf (widget-width-request fixed) (+ w (* 2 *canvas-margin*))
+		     (widget-height-request fixed) (+ h (* 2 *canvas-margin*)))
+	       (setf (fixed-child-x fixed da0) *canvas-margin*
+		     (fixed-child-y fixed da0) *canvas-margin*)
+	       (setf backing-pixmap (pixmap-new backing-pixmap w h -1))
+	       (when pixbuf
+		 (draw-pixbuf backing-pixmap gc pixbuf
+			      0 0 0 0 -1 -1 :none 0 0)))
 	     (file-load (a)
 	       (declare (ignore a))
 	       (let ((d (make-instance 'file-chooser-dialog :action :open :title "Choose file to open")))
 		 (dialog-add-button d "gtk-open" :accept)
 		 (dialog-add-button d "gtk-cancel" :cancel)
 		 (when (eq (dialog-run d) :accept)
-		   (let* ((gc (graphics-context-new pixmap))
+		   (let* ((gc (graphics-context-new backing-pixmap))
 			  (pixbuf (pixbuf-new-from-file (file-chooser-filename d)))
 			  (w (pixbuf-width pixbuf))
 			  (h (pixbuf-height pixbuf)))
-		     (setf (widget-width-request da0) w
-			   (widget-height-request da0) h)
-		     (setf (widget-width-request fixed) (+ w (* 2 *canvas-margin*))
-			   (widget-height-request fixed) (+ h (* 2 *canvas-margin*)))
-		     (setf (fixed-child-x fixed da0) *canvas-margin*
-			   (fixed-child-y fixed da0) *canvas-margin*)
-		     (setf pixmap (pixmap-new pixmap w h -1))
-		     (draw-pixbuf pixmap gc pixbuf
-				  0 0 0 0 -1 -1 :none 0 0)
-		     (format *debug* "saved to file ~A~%" (file-chooser-filename d))))
+		     (change-canvas-size w h gc pixbuf)))
 		 (object-destroy d)))
 	     (create-input-dialog ()
 	       (unless input-d
@@ -195,6 +221,8 @@
 	  (setf (adjustment-value spacing-adjustment) 2d0)
 	  (setf alpha-adjustment (bgo "brush-alpha-adjustment"))
 	  (setf viewport (bgo "viewport1"))
+	  (setf vadjustment (viewport-vadjustment viewport)
+		hadjustment (viewport-hadjustment viewport))
 	  (print (adjustment-lower (viewport-vadjustment viewport)) *debug*)
 	  (box-pack-start v-box-2 h-s-v :expand nil :fill nil)
 	  (setf hsv h-s-v)
@@ -223,6 +251,8 @@
 								     (widget-hide new-painting-dialog)))
 	  (connect-signal (bgo "save-action") "activate" #'file-save)
 	  (connect-signal (bgo "open-action") "activate" #'file-load)
+	  (connect-signal (bgo "zoom-in-action") "activate"  #'(lambda (a) (declare (ignore a)) (print (zoom-in zoomer) *debug*)))
+	  (connect-signal (bgo "zoom-out-action") "activate"  #'(lambda (a) (declare (ignore a)) (print (zoom-out zoomer) *debug*)))
 	  (connect-signal (bgo "options-action") "activate" #'(lambda (a) (declare (ignore a)) (create-input-dialog)))
 	  (connect-signal (bgo "quit-action") "activate" #'(lambda (action)
 						      (declare (ignore action))
